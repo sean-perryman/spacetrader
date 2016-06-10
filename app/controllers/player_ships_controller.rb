d45fa261ee57351688210fbcfc52fbd9e39f1f4f
Sean Perryman <se@nperryman.com>
2016-06-09 17:19:55 -0700

    Working on ship items

--- app/controllers/player_ships_controller.rb
+++ app/controllers/player_ships_controller.rb
@@ -1,22 +1,31 @@
 class PlayerShipsController < ApplicationController
+  before_action :set_player_ship, only: [:show, :edit, :update, :destroy]
+
   def index
   	@player_ships = PlayerShip.all
   end
 
   def show
+    @ship_items = ShipItem.where(:ship_id => @player_ship.ship)
   end
 
   def edit
   end
 
+  def update
+  end
+
+  def destroy
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_item
+    def set_player_ship
       @player_ship = PlayerShip.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def player_ships_params
-      params.require(:player_ship).permit(:Name, :Base_Price)
+      params.require(:player_ship).permit(:player, :ship, :cargo_mod)
     end
 end
